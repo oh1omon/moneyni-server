c261b41e34930b84cfb2cb5dc523d799e6ae72b2
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-27 10:09:05 +0300

    reorganized project a bit, according to new knowdledge

--- db/database.ts
+++ db/database.ts
@@ -1,166 +1,143 @@
-import mongoose, { Types } from 'mongoose';
-import bcrypt from 'bcrypt';
-import { IUserDocument, IUserInput } from './user/user.types';
-import { UserModel } from './user/user.model';
-import { ISpendDocument, ISpendInput } from './spends/spend.types';
-import { SpendModel } from './spends/spend.model';
+import bcrypt from 'bcrypt'
+import mongoose, { Types } from 'mongoose'
+import { SpendModel } from './spends/spend.model'
+import { ISpendDocument, ISpendInput } from './spends/spend.types'
+import { UserModel } from './user/user.model'
+import { IUserDocument, IUserInput } from './user/user.types'
 
-export let database: mongoose.Connection;
+export let database: mongoose.Connection
 
 export const connect = () => {
-    const uri = process.env.DB_CONNECT_LINK;
+	if (database) {
+		return
+	}
 
-    if (database) {
-        return;
-    }
+	mongoose.connect(process.env.DB_CONNECT_LINK, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
 
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    });
+	database = mongoose.connection
 
-    database = mongoose.connection;
+	database.once('open', async () => {
+		console.log('Connected to database')
+	})
 
-    database.once('open', async () => {
-        console.log('Connected to database');
-    });
-
-    database.on('error', () => {
-        console.log('Error connecting to database');
-    });
-};
+	database.on('error', () => {
+		console.log('Error connecting to database')
+	})
+}
 
 /**
  * @param {IUserInput} newUser object, containing fields of email, password, name
  * @return either error message or new User Object
  * */
-export const createNewUser = async (
-    newUser: IUserInput
-): Promise<IUserDocument> => {
-    return new Promise(async (resolve, reject) => {
-        //Checking if all data is presented to registration
-        if (!newUser) {
-            reject('Please insert data!');
-            return;
-        }
-        //Checking if there is an user with such email
-        const foundUser = await UserModel.findOne({ email: newUser.email });
-        if (foundUser) {
-            reject('There are an account with such email already');
-            return;
-        }
-        //Hashing password
-        const hashedPassword: string = await bcrypt.hash(newUser.password, 10);
-        //Create new USer
-        const injectedUser: IUserDocument = await UserModel.create({
-            _id: new mongoose.Types.ObjectId(),
-            email: newUser.email,
-            password: hashedPassword,
-            name: newUser.name,
-            spendings: [],
-        });
-        //Working with the result
-        injectedUser
-            ? resolve(injectedUser)
-            : reject('Something went wrong...');
-    });
-};
+export const createNewUser = async (newUser: IUserInput): Promise<IUserDocument> => {
+	return new Promise(async (resolve, reject) => {
+		//Checking if all data is presented to registration
+		if (!newUser) {
+			reject('Please insert data!')
+			return
+		}
+		//Checking if there is an user with such email
+		const foundUser = await UserModel.findOne({ email: newUser.email })
+		if (foundUser) {
+			reject('There are an account with such email already')
+			return
+		}
+		//Hashing password
+		const hashedPassword: string = await bcrypt.hash(newUser.password, 10)
+		//Create new USer
+		const injectedUser: IUserDocument = await UserModel.create({
+			_id: new mongoose.Types.ObjectId(),
+			email: newUser.email,
+			password: hashedPassword,
+			name: newUser.name,
+			spendings: [],
+		})
+		//Working with the result
+		injectedUser ? resolve(injectedUser) : reject('Something went wrong...')
+	})
+}
 
-export const getUserByEmail = async (
-    email: string
-): Promise<IUserDocument | undefined> => {
-    return new Promise(async (resolve, reject) => {
-        if (!email) {
-            reject('No email provided');
-            return;
-        }
-        UserModel.findOne(
-            { email },
-            (err: Error, data: IUserDocument | null) => {
-                if (err) {
-                    reject(err);
-                    return;
-                } else {
-                    resolve(data);
-                }
-            }
-        );
-    });
-};
+export const getUserByEmail = async (email: string): Promise<IUserDocument | undefined> => {
+	return new Promise(async (resolve, reject) => {
+		if (!email) {
+			reject('No email provided')
+			return
+		}
+		UserModel.findOne({ email }, (err: Error, data: IUserDocument | null) => {
+			if (err) {
+				reject(err)
+				return
+			} else {
+				resolve(data)
+			}
+		})
+	})
+}
 
-export const getUserById = async (
-    id: string
-): Promise<IUserDocument | undefined> => {
-    return new Promise(async (resolve, reject) => {
-        if (!id) {
-            reject('No id provided');
-            return;
-        }
-        UserModel.findOne(
-            { _id: id },
-            (err: Error, data: IUserDocument | null) => {
-                if (err) {
-                    reject(err);
-                    return;
-                } else {
-                    resolve(data);
-                }
-            }
-        );
-    });
-};
+export const getUserById = async (id: string): Promise<IUserDocument | undefined> => {
+	return new Promise(async (resolve, reject) => {
+		if (!id) {
+			reject('No id provided')
+			return
+		}
+		UserModel.findOne({ _id: id }, (err: Error, data: IUserDocument | null) => {
+			if (err) {
+				reject(err)
+				return
+			} else {
+				resolve(data)
+			}
+		})
+	})
+}
 
 //Spend Operations
 
-export const getSpendsById = async (
-    idArr: Types.ObjectId[] | []
-): Promise<ISpendDocument | ISpendDocument[] | string> => {
-    return new Promise(async (resolve, reject) => {
-        //Finding all spends by idArr
-        const foundDocs = await SpendModel.find({
-            _id: { $in: idArr },
-        });
-        if (foundDocs) {
-            resolve(foundDocs);
-            return;
-        } else {
-            reject('none found focs');
-            return;
-        }
-    });
-};
+export const getSpendsById = async (idArr: Types.ObjectId[] | []): Promise<ISpendDocument | ISpendDocument[] | string> => {
+	return new Promise(async (resolve, reject) => {
+		//Finding all spends by idArr
+		const foundDocs = await SpendModel.find({
+			_id: { $in: idArr },
+		})
+		if (foundDocs) {
+			resolve(foundDocs)
+			return
+		} else {
+			reject('none found focs')
+			return
+		}
+	})
+}
 
-export const addNewSpend = async (
-    newSpend: ISpendInput
-): Promise<ISpendDocument | string> => {
-    return new Promise(async (resolve, reject) => {
-        //Trying to insert new Spend
-        const addedSpend = SpendModel.create({
-            _id: new mongoose.Types.ObjectId(),
-            category: newSpend.category,
-            comment: newSpend.comment,
-            cost: newSpend.cost,
-            currency: newSpend.currency,
-        });
-        //Working with the result
-        addedSpend ? resolve(addedSpend) : reject('Something went wrong');
-    });
-};
+export const addNewSpend = async (newSpend: ISpendInput): Promise<ISpendDocument | string> => {
+	return new Promise(async (resolve, reject) => {
+		//Trying to insert new Spend
+		const addedSpend = SpendModel.create({
+			_id: new mongoose.Types.ObjectId(),
+			category: newSpend.category,
+			comment: newSpend.comment,
+			cost: newSpend.cost,
+			currency: newSpend.currency,
+		})
+		//Working with the result
+		addedSpend ? resolve(addedSpend) : reject('Something went wrong')
+	})
+}
 
-export const addNewSpendToUser = async (
-    userId: Types.ObjectId,
-    newSpend: Types.ObjectId
-): Promise<IUserDocument | string> => {
-    return new Promise(async (resolve, reject) => {
-        const updatedUser = await UserModel.findOneAndUpdate(
-            { _id: userId },
-            { $push: { spendings: newSpend } },
-            {
-                new: true,
-            }
-        );
-        updatedUser ? resolve(updatedUser) : reject('Something went wrongi');
-    });
-};
+export const addNewSpendToUser = async (userId: Types.ObjectId, newSpend: Types.ObjectId): Promise<IUserDocument | string> => {
+	return new Promise(async (resolve, reject) => {
+		const updatedUser = await UserModel.findOneAndUpdate(
+			{ _id: userId },
+			{ $push: { spendings: newSpend } },
+			{
+				new: true,
+			}
+		)
+		updatedUser ? resolve(updatedUser) : reject('Something went wrongi')
+	})
+}
--- index.ts
+++ index.ts
@@ -1,78 +1,66 @@
-import express from 'express';
-import session from 'express-session';
-import cors from 'cors';
-import * as dotenv from 'dotenv';
-dotenv.config();
-import { connect } from './db/database';
-import passport from 'passport';
-import signUp from './routes/signUp';
-import { default as connectMongoDBSession } from 'connect-mongodb-session';
-import signIn from './routes/signIn';
-import path from 'path';
-import signOut from './routes/signOut';
-import addSpend from './routes/addSpend';
-import getSpends from './routes/getSpends';
-import authConfirm from './routes/authConfirm';
-import addSpendToUser from './routes/addSpendToUser';
-const MongoDBStore = connectMongoDBSession(session);
+import MongoStore from 'connect-mongo'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import express from 'express'
+import session from 'express-session'
+import passport from 'passport'
+import { connect } from './db/database'
+import addSpend from './routes/addSpend'
+import addSpendToUser from './routes/addSpendToUser'
+import authConfirm from './routes/authConfirm'
+import getSpends from './routes/getSpends'
+import signIn from './routes/signIn'
+import signOut from './routes/signOut'
+import signUp from './routes/signUp'
+dotenv.config()
 
 //Extracting PORT & HOST variables from .env file
-const PORT: number = parseInt(process.env.PORT as string, 10);
-const HOST: string = process.env.HOST!;
+const PORT: number = parseInt(process.env.PORT as string, 10)
 
 //Connecting to MongoDB Atlas
-connect();
+connect()
 
 //Initializing Express
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 //Bodyparser Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// //Adding flash
-// app.use(flash());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 //Initializing Express session
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoDBStore({
-            uri: process.env.DB_CONNECT_LINK,
-            collection: 'mySessions',
-        }),
-        cookie: { maxAge: 60 * 60 * 1000 * 24 * 30 },
-    })
-);
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({
+			mongoUrl: process.env.DB_CONNECT_LINK,
+			stringify: false,
+		}),
+		cookie: { maxAge: 60 * 60 * 1000 * 24 * 30 },
+	})
+)
 
 //Applying Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Routes
 
-// Only for testing
-app.use(express.static(path.join(__dirname, 'build')));
-
 //Auth Handlers
-app.use('/api/signup', signUp);
-app.use('/api/signin', signIn);
-app.use('/api/signout', signOut);
-app.use('/api/authconfirm', authConfirm);
+app.use('/api/signup', signUp)
+app.use('/api/signin', signIn)
+app.use('/api/signout', signOut)
+app.use('/api/authconfirm', authConfirm)
 
 //Adding, Getting Spends
-app.use('/api/addspend', addSpend);
-app.use('/api/getspends', getSpends);
-app.use('/api/addspendtouser', addSpendToUser);
+app.use('/api/addspend', addSpend)
+app.use('/api/getspends', getSpends)
+app.use('/api/addspendtouser', addSpendToUser)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
-});
 //Starting server
-app.listen(PORT, HOST, () => {
-    console.log(`server is listening on ${HOST}:${PORT}`);
-});
+app.listen(PORT, () => {
+	console.log(`server is listening on ${PORT}`)
+})
