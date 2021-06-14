dee25598ddccf8bdacbde63ed21d6afda57416a0
Artem Zagarov <artem.zagarov@gmail.com>
2021-06-14 16:43:36 +0300

    Changed spend Schema
    Added validations to its fields

--- models/spend-schema.ts
+++ models/spend-schema.ts
@@ -3,10 +3,34 @@ import { ISpendDocument } from '../types'
 
 export const spendSchema: Schema = new Schema({
 	_id: Types.ObjectId,
-	category: String,
-	comment: String,
-	cost: Number,
-	currency: String,
+	category: {
+		type: String,
+		required: [true, 'Category is needed'],
+		enum: {
+			values: [
+				'Daily Needs',
+				'Bad Habits',
+				'Hygiene and Health',
+				'Housing',
+				'Clothing and Cosmetics',
+				'Travel',
+				'Food',
+				'Entertainment and Gifts',
+				'Connection',
+			],
+			message: '{VALUE} is not supported as spend type',
+		},
+	},
+	comment: { type: String },
+	cost: { type: Number, required: [true, 'Cost is needed'], max: 350_000 },
+	currency: {
+		type: String,
+		required: [true, 'Currency is needed'],
+		enum: {
+			values: ['€', '$', ' ₽'],
+			message: '{VALUE} is not supported as currency',
+		},
+	},
 })
 
 export const Spend: Model<ISpendDocument> = model<ISpendDocument>('Spend', spendSchema)
